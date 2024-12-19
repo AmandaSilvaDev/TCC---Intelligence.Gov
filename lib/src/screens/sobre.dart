8f2b5e5710d6fe1a22551086a07cf6df01f683b3
AmandaSilvaDev <125752965+AmandaSilvaDev@users.noreply.github.com>
2024-12-18 21:02:33 -0300

    texto sobre o projeto

--- lib/src/screens/sobre.dart
+++ lib/src/screens/sobre.dart
@@ -46,7 +46,8 @@ class About extends StatelessWidget {
               ),
               // Texto descritivo sobre o aplicativo.
               const Text(
-                'É um aplicativo móvel projetado para oferecer aos usuários acesso fácil e estratégico das informações detalhadas sobre os deputados da Câmara dos Deputados do Brasil. Esperamos que ajude a fiscalizar nossos deputados e melhorar o senso crítico para o voto.',
+                'Este aplicativo foi desenvolvido como parte do Trabalho de Conclusão de Curso (TCC) no curso de Análise e Desenvolvimento de Sistemas, turma de 2022, do Instituto Federal de Educação, Ciência e Tecnologia de Rondônia (IFRO). O aplicativo tem como objetivo fornecer aos usuários acesso de forma prática e estratégica a informações detalhadas sobre os deputados da Câmara dos Deputados do Brasil. Espera-se que a plataforma contribua para a fiscalização das atividades parlamentares, promovendo uma maior conscientização e senso crítico por parte dos cidadãos, e, assim, auxiliando na formação de um voto mais informado e responsável.'
+                ,
                 textAlign: TextAlign.center, // Alinha o texto ao centro.
                 style: TextStyle(
                   fontSize: 18, // Define o tamanho da fonte.
@@ -68,7 +69,7 @@ class About extends StatelessWidget {
                   CircleAvatar(
                     radius: 50,
                     backgroundImage:  NetworkImage(
-                      'https://github.com/AmandaSilvaDev/Intelligence.IFRO/blob/main/assets/images/Amanda.jpeg?raw=true',
+                      'https://raw.githubusercontent.com/AmandaSilvaDev/TCC---Intelligence.Gov/refs/heads/main/assets/images/perfil.jpg',
                     ),
                   ),
                   SizedBox(height: 10), // Espaçamento entre widgets.
